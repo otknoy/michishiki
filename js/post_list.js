fde6c5357f3200aca55c7c0ccba932c20c1de4d7
otknoy <otknoy@gmail.com>
2014-07-27 18:25:48 +0900

    Implement post list

---
+++ js/post_list.js
@@ -0,0 +1,18 @@
+$(function() {
+    $.getJSON('http://amateras.wsd.kutc.kansai-u.ac.jp/~otsuka/michishiki_api_server/select.py',
+	      function(json) {
+		  for (var i = 0; i < json.length; i++) {
+		      var $li = createListItem(json[i].title, json[i].comment, json[i].posted_by);
+		      $('#post_list').append($li);
+		  }
+		  $('#post_list').listview('refresh');
+	      });
+});
+
+function createListItem(title, comment, posted_by) {
+    var $title = $('<h2>').text(title);
+    var $comment = $('<p>').text(comment);
+    var $posted_by = $('<p>').text(posted_by);
+    var $li = $('<li>').append($title).append($comment).append($posted_by);
+    return $li
+}
