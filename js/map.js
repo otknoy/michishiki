5f43d627c552eaa725447b327e201fc1439c6073
otknoy <otknoy@gmail.com>
2014-07-30 21:46:15 +0900

    Minor change

--- js/map.js
+++ js/map.js
@@ -1,11 +1,17 @@
+// var api_uri = 'http://mpmc.dip.jp/~otknoy/michishiki_api_server/select.py';
+var api_uri = 'http://localhost/~otknoy/michishiki_api_server/select.py';
+
 var map = null;
+var markers = null;
+var infoWindow = null;
 
 $(document).on('pageshow', '#map', function() {
-    // Draw the map once
-    if (map == null) initMap();
+    markers = [];
+    map = createMap();
+    infoWindow = new google.maps.InfoWindow;
 });
 
-function initMap() {
+function createMap() {
     $('#map_canvas').css('height', 500);
 
     var latlng = new google.maps.LatLng(34.87728, 135.576798); // kutc
@@ -14,15 +20,17 @@ function initMap() {
 	zoom: 8,
 	mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+    var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
     // Get posts and create markers
-    var api_uri = 'http://mpmc.dip.jp/~otknoy/michishiki_api_server/select.py';
     $.getJSON(api_uri, function(json) {
 	for (var i = 0; i < json.length; i++) {
-	    createMarker(map, json[i].title, json[i].latitude, json[i].longitude);
+	    var marker = createMarker(map, json[i].title, json[i].latitude, json[i].longitude);
+	    markers.push(marker);
 	}
     });
+
+    return map;
 }
 
 function createMarker(map, title, latitude, longitude) {
