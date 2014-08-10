3e17630fae9dc9e3bd8c88aafc1af7b569979919
otknoy <otknoy@gmail.com>
2014-08-11 00:09:36 +0900

    Refactoring

--- js/map.js
+++ js/map.js
@@ -1,55 +1,52 @@
-var map = {};
-map.map = null;
-map.markers = null;
+var Map = {};
+Map.map = null;
+Map.markers = null;
 
 $(document).on('pageshow', '#map', function() {
     initMap();
 });
 
 function initMap() {
-    map.markers = [];
-    map.map = createMap();
+    Map.markers = [];
 
-    utils.getCurrentLocation()
-	.done(function(location) {
-	    var pos = new google.maps.LatLng(location.latitude,
-					     location.longitude);
-	    map.map.setCenter(pos);
-
-	    var marker = createMarker(map, 'Current location', pos.latitude, pos.longitude);
-	    map.markers.push(marker);
-	});
-}
-
-function createMap() {
     $('#map-canvas').css('height', 500);
-
-    var latlng = new google.maps.LatLng(34.87728, 135.576798); // kutc
-    var mapOptions = {
-	center: latlng,
-	zoom: 8,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    Map.map = Map.createMap('map-canvas', 34.87728, 135.576798); // kutc
 
     // Get posts and create markers
     var api_uri = 'http://amateras.wsd.kutc.kansai-u.ac.jp/~otsuka/michishiki_api_server/select.py';
     $.getJSON(api_uri, function(json) {
 	for (var i = 0; i < json.length; i++) {
-	    var marker = createMarker(map, json[i].title, json[i].latitude, json[i].longitude);
-	    map.markers.push(marker);
+	    var marker = Map.createMarker(Map.map, json[i].title, json[i].latitude, json[i].longitude);
+	    Map.markers.push(marker);
 	}
     });
 
-    return map;
+    utils.getCurrentLocation()
+	.done(function(location) {
+	    var pos = new google.maps.LatLng(location.latitude,
+					     location.longitude);
+	    Map.map.setCenter(pos);
+
+	    var marker = Map.createMarker(Map.map, 'Current location', pos.latitude, pos.longitude);
+	    Map.markers.push(marker);
+	});
 }
 
-function createMarker(map, title, latitude, longitude) {
-    var latlng = new google.maps.LatLng(latitude, longitude);
+Map.createMap = function(id, lat, lng) {
+    var mapOptions = {
+	center: new google.maps.LatLng(lat, lng),
+	zoom: 8,
+	mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById(id), mapOptions);
+    return map;
+};
+
+Map.createMarker = function(map, title, lat, lng) {
     var marker = new google.maps.Marker({
-	position: latlng,
+	position: new google.maps.LatLng(lat, lng),
 	map: map,
 	title : title
     });
     return marker;
-}
+};
