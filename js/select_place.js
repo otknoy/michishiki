3d49beba240e1dca8fe992b4b433f8f6e720442d
otknoy <otknoy@gmail.com>
2014-08-11 20:23:16 +0900

    Post location selection

--- js/select_place.js
+++ js/select_place.js
@@ -8,6 +8,12 @@ $(document).on('pageshow', '#select-place', function() {
 				  location.latitude, location.longitude);
 	Place.currentLocation = Map.createMarker(Place.map, 'Current Location',
 						 location.latitude, location.longitude, true);
+
+	Place.setPostLocation();
+
+	google.maps.event.addListener(Place.currentLocation, 'dragend', function() {
+	    Place.setPostLocation();
+	});
     });
 });
 
@@ -16,4 +22,10 @@ Place.getMarkerLocation = function() {
     var lat = pos.lat();
     var lng = pos.lng();
     return {"latitude": lat, "longitude": lng};
+};
+
+Place.setPostLocation = function() {
+    var location = Place.getMarkerLocation();
+    $('#post-lat').val(location.latitude);
+    $('#post-lng').val(location.longitude);
 };
