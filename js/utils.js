2d56f9a86dce871e49db22821dd63181d2856b5b
otknoy <otknoy@gmail.com>
2014-08-14 11:01:49 +0900

    Available query string for filtering result of fetch posts

--- js/utils.js
+++ js/utils.js
@@ -28,13 +28,23 @@ utils.getCurrentLocation = function() {
     return dfd.promise();
 };
 
-utils.fetchPosts = function() {
+utils.fetchPosts = function(options) {
     var dfd = $.Deferred();
 
     var api_uri = 'http://amateras.wsd.kutc.kansai-u.ac.jp/~otsuka/michishiki_api_server/select.py';
-    $.getJSON(api_uri, function(json) {
+    var query_string = utils.optionsToQueryString(options);
+    $.getJSON(api_uri + query_string, function(json) {
 	dfd.resolve(json);
     });
 
     return dfd.promise();
+};
+
+utils.optionsToQueryString = function(options) {
+    var querys = [];
+    for (var key in options) {
+	var q = key + '=' + options[key];
+	querys.push(q);
+    }
+    return '?' + querys.join('&');
 };
