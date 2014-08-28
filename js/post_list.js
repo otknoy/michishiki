8cbc93303f8ac03ca3845cd4c086bda2f4290790
otknoy <otknoy@gmail.com>
2014-08-28 09:44:39 +0900

    Add date in listview item

--- js/post_list.js
+++ js/post_list.js
@@ -6,17 +6,24 @@ function initPostList() {
     utils.fetchPosts(queryOption).done(function(json) {
 	$('#post_list').empty();
 	for (var i = 0; i < json.length; i++) {
-	    var $li = createListItem(json[i].title, json[i].comment, json[i].posted_by);
+	    var $li = createListItem(json[i].title,
+				     json[i].posted_by,
+				     utils.utcToString(json[i].created_at),
+				     json[i].comment);
 	    $('#post_list').append($li);
 	}
 	$('#post_list').listview('refresh');
     });
 }
 
-function createListItem(title, comment, posted_by) {
+function createListItem(title, writer, date, comment) {
     var $title = $('<h2>').text(title);
+    var $writer = $('<p>').text(writer);
+    var $date = $('<p>').text(date);
     var $comment = $('<p>').text(comment);
-    var $posted_by = $('<p>').text(posted_by);
-    var $li = $('<li>').append($title).append($comment).append($posted_by);
-    return $li;
+    return $('<li>')
+	.append($title)
+	.append($writer)
+	.append($date)
+	.append($comment);
 }
