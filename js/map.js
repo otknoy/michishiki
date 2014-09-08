80e63e4bed46cb5fc1f09e63af41e2735a7fd9ca
otknoy <otknoy@gmail.com>
2014-09-08 16:06:04 +0900

    Add map bounds_changed event listener after creating map

--- js/map.js
+++ js/map.js
@@ -10,15 +10,15 @@ Map.initMap = function() {
     $('#map-canvas').css('height', 500);
 
     utils.getCurrentLocation().done(function(location) {
-	Map.map = Map.createMap(location.latitude, location.longitude, 8);
+	Map.map = Map.createMap('map-canvas', location.latitude, location.longitude, 8);
 	Map.currentLocation = Map.createMarker(Map.map, 'Current location',
 					       location.latitude, location.longitude, false);
-    });
 
-    Map.infoWindow = new google.maps.InfoWindow;
+	Map.infoWindow = new google.maps.InfoWindow;
 
-    google.maps.event.addListener(Map.map, 'bounds_changed', function() {
-	utils.fetchPosts(Map.buildOption()).done(Map.updateMarkers);
+	google.maps.event.addListener(Map.map, 'bounds_changed', function() {
+	    utils.fetchPosts(Map.buildOption()).done(Map.updateMarkers);
+	});
     });
 };
 
