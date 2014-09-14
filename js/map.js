7d2f08c9bb5866b52f2c4e05b285e6e259bfc692
otknoy <otknoy@gmail.com>
2014-09-14 16:02:57 +0900

    Initialize in michishiki.js

--- js/map.js
+++ js/map.js
@@ -1,26 +1,6 @@
 var Map = {};
 Map.markers = [];
 
-$(document).on('pageshow', '#jimoto-map', function() {
-    Map.initMap();
-});
-
-Map.initMap = function() {
-        $('#map-canvas').css('height', 500);
-
-    utils.getCurrentLocation().done(function(location) {
-	Map.map = Map.createMap('map-canvas', location.latitude, location.longitude, 8);
-	Map.currentLocation = Map.createMarker(Map.map, 'Current location',
-					       location.latitude, location.longitude, false);
-
-	Map.infoWindow = new google.maps.InfoWindow;
-
-	google.maps.event.addListener(Map.map, 'bounds_changed', function() {
-	    utils.fetchPosts(Map.buildOption()).done(Map.updateMarkers);
-	});
-    });
-};
-
 Map.buildOption = function() {
     var bounds = Map.getMapBounds(Map.map);
     var options = new QueryOptionBuilder()
---
+++ js/michishiki.js
@@ -0,0 +1,21 @@
+var michishiki = {};
+
+$(document).on('pageshow', '#main-map', function() {
+    initMap();
+});
+
+initMap = function() {
+    $('#map-canvas').css('height', 450);
+
+    utils.getCurrentLocation().done(function(location) {
+	Map.map = Map.createMap('map-canvas', location.latitude, location.longitude, 8);
+	Map.currentLocation = Map.createMarker(Map.map, 'Current location',
+					       location.latitude, location.longitude, false);
+
+	Map.infoWindow = new google.maps.InfoWindow;
+
+	google.maps.event.addListener(Map.map, 'bounds_changed', function() {
+	    utils.fetchPosts(Map.buildOption()).done(Map.updateMarkers);
+	});
+    });
+};
