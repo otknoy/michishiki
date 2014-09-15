3c21ca7fca020bf80255ecf742e81170be4ecb3b
otknoy <otknoy@gmail.com>
2014-09-15 12:03:56 +0900

    Enable post page

--- js/select_place.js
+++ js/select_place.js
@@ -1,10 +1,10 @@
 var Place = {};
 
 $(document).on('pageshow', '#select-place', function() {
-    $('#select-place-map-canvas').css('height', 500);
+    $('#select-place-map').css('height', 500);
 
     utils.getCurrentLocation().done(function(location) {
-	Place.map = Map.createMap('select-place-map-canvas',
+	Place.map = Map.createMap('select-place-map',
 				  location.latitude, location.longitude, 12);
 	Place.currentLocation = Map.createMarker(Place.map, 'Current Location',
 						 location.latitude, location.longitude, true);
