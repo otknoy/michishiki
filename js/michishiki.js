7bc3e09a1e29bffa9441c96ff58cd45f08c38568
otknoy <otknoy@gmail.com>
2014-09-26 10:59:50 +0900

    Refactoring

--- js/michishiki.js
+++ js/michishiki.js
@@ -34,6 +34,38 @@ $(document).on('pageshow', '#select-place', function() {
     michishiki.initPlaceSelectMap();
 });
 
+$(document).on('pageinit', '#post', function() {
+    $('#post_form').submit(function(e) {
+	e.preventDefault();
+
+	var $form = $('#post_form');
+	var $button = $form.find('#post_button');
+	$.ajax({
+            url: michishiki.api.uri + 'post.py',
+            type: 'post',
+            data: $form.serialize(),
+	    timeout: 10000,
+
+	    beforeSend: function(xhr, settings) {
+		$button.attr('disabled', true);
+	    },
+
+	    complete: function(xhr, textStatus) {
+		$button.attr('disabled', false);
+	    },
+
+	    success: function(data, textStatus, xhr) {
+		$form[0].reset();
+	    },
+
+	    error: function(xhr, textStatus, error) {
+		alert('error');
+		console.log(error);
+            }
+	});
+    });
+});
+
 michishiki.initMainMap = function() {
     $('#map-canvas').css('height', 450);
 
